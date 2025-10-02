7b1fff62e7fd0ddf3a0a5cf182112c946c75df89
Bacem <bacemjarraya@yahoo.fr>
2025-10-02 15:30:18 +0100

    Adding user Item

--- page-components/documents/navigation.tsx
+++ page-components/documents/navigation.tsx
@@ -1,12 +1,13 @@
 import CollapseButton from '@/components/ui/collapse-button'
 
 import React from 'react'
+import UserItem from './user-item'
 
 function Navigation() {
   return (
   <aside className='group/sidebar h-full v-full bg-secondary overflow-y-auto  flex flex-col z-[99999] min-h-[100vh] left-0 top-0'>
-    <CollapseButton className='mt-2 ml-auto hover:bg-white/90 hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background rounded-md transition-colors' />
-        navigation
+    <CollapseButton className='absolute top-3 right-2 hover:bg-white/90 hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background rounded-md transition-colors' />
+        <UserItem />
     </aside>
   )
 }
