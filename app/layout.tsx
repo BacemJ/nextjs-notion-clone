b653be23fb11b813294bd38e28c084afe53d7b7d
Bacem <bacemjarraya@yahoo.fr>
2025-10-02 17:31:21 +0100

    todo create document and get document

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -59,6 +60,7 @@ export default function RootLayout({
           >
             <ConvexClientProvider>{children}</ConvexClientProvider>
           </ThemeProvider>
+          <Toaster position="bottom-right" />
         </body>
       </html>
     </ClerkProvider>
